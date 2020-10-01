7d2f9ed9f98f344b910a36ef39ee44526cfc7825
Javier Perez <maganaperezjavier2@gmail.com>
2020-10-01 14:28:51 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Code Challenge 1
+Javier Perez 10/01/2020
+This program finds the first, 7 (or n) digit, prime, palindromic number in the digits of Pi
+Written in Python3 v3.8.1
--- bang.py
+++ bang.py
@@ -1,4 +1,4 @@
-# Javier Perez 09/27/2020
+# Javier Perez 10/01/2020
 # This program finds the first, 7 (or n) digit, prime, palindromic number in the digits of Pi
 # Written in Python3 v3.8.1
 
